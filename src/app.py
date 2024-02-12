d61b2c15ad0f1b84c2dc94b4e82dc162483bc0b9
Ciro <cironidas@gmail.com>
2024-02-12 14:25:46 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,41 +1,40 @@
 import openai
 import streamlit as st
 
-# Configuración de OpenAI
+
+
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 openai.api_base_url = "https://acc-alejandria-core-openaimagesound-pro.openai.azure.com"
 openai.api_version = "2023-07-01-preview"
 openai.api_type = "azure"
 
-# Parámetros del chatbot
+EMBEDDING_MODEL = "text-embedding-ada-002"
+GPT_EMBEDDING_ENGINE = 'mondongodb'
+DIMENSION = 1536
 GPT_MODEL = 'gpt-3.5-turbo-16k'
 GPT_CHAT_ENGINE = "gepeto"
+# GPT_MODEL = 'gpt-4'
+# GPT_CHAT_ENGINE = "dictador"
+
 
 st.title("Chatbot")
 
-# Verificar si existe la lista de mensajes en la sesión
 if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+  st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
 
-# Mostrar todos los mensajes en la lista
 for msg in st.session_state["messages"]:
-    st.write(f"{msg['role']}: {msg['content']}")
+  st.chat_message(msg["role"]).write(msg["content"])
 
-# Esperar la entrada del usuario
-user_input = st.text_input("Usted:", "")
+if user_input := st.chat_input():
+  st.session_state["messages"].append({"role": "user", "content": user_input})
+  st.chat_message("user").write(user_input)
 
-# Si se proporciona una entrada de usuario, generar la respuesta del bot
-if user_input:
-    # Agregar el mensaje del usuario a la lista
-    st.session_state["messages"].append({"role": "Usted", "content": user_input})
-
-    # Generar la respuesta del bot con OpenAI
-    response = openai.ChatCompletion.create(
+  response = openai.ChatCompletion.create(
         model=GPT_MODEL,
         messages=st.session_state["messages"],
         engine=GPT_CHAT_ENGINE
     )
   
-    # Agregar la respuesta del bot a la lista
-    responseMessage = response['choices'][0]['message']['content']
-    st.session_state["messages"].append({"role": "Bot", "content": responseMessage})
+  responseMessage = response['choices'][0]['message']['content']
+  st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
+  st.chat_message("assistant").write(responseMessage)
