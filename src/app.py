07e1439c874ada50ffd4c789d7b0915a4160dba4
Ciro <cironidas@gmail.com>
2024-02-12 12:31:12 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -32,11 +32,13 @@ for msg in st.session_state["messages"]:
 if user_input := st.chat_input():
   st.session_state["messages"].append({"role": "user", "content": user_input})
   st.chat_message("user").write(user_input)
+
   response = client.chat.completions.create(
         model=GPT_MODEL,
         messages=st.session_state["messages"],
         engine=GPT_CHAT_ENGINE
     )
+  
   responseMessage = response['choices'][0]['message']['content']
   st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
   st.chat_message("assistant").write(responseMessage)
