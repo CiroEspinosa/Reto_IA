52cbc7507106d998db07ad4c66e00ab8e278ccfd
Ciro <cironidas@gmail.com>
2024-02-12 12:29:14 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
@@ -6,10 +6,12 @@ import streamlit as st
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_base_url = os.getenv("OPENAI_API_BASE")
-openai.api_version = os.getenv("OPENAI_API_VERSION")
-openai.api_type = os.getenv("OPENAI_API_TYPE")
+client = OpenAI(
+  api_key = os.getenv("OPENAI_API_KEY"),
+  api_base_url = os.getenv("OPENAI_API_BASE"),
+  api_version = os.getenv("OPENAI_API_VERSION"),
+  api_type = os.getenv("OPENAI_API_TYPE")
+)
 
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_EMBEDDING_ENGINE = 'mondongodb'
@@ -30,7 +32,7 @@ for msg in st.session_state["messages"]:
 if user_input := st.chat_input():
   st.session_state["messages"].append({"role": "user", "content": user_input})
   st.chat_message("user").write(user_input)
-  response = openai.ChatCompletion.create(
+  response = client.chat.completions.create(
         model=GPT_MODEL,
         messages=st.session_state["messages"],
         engine=GPT_CHAT_ENGINE
