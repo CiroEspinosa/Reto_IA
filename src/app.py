78a4a9a14f04c46f462cb282bfec0891d8449370
Ciro <cironidas@gmail.com>
2024-02-14 12:23:37 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -41,14 +41,14 @@ is_pdf_chatbot = st.checkbox("PDF chatbot")
 
 if is_pdf_chatbot:
   uploaded_file = st.file_uploader("Sube tu archivo PDF", type="pdf")
-
+  st.session_state["messages"].append({"role": "assistant", "content": "Mierda"})
   if uploaded_file is not None:
       pdf_bytes = uploaded_file.read()
       pdf_file = BytesIO(pdf_bytes)
       docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME, DIMENSION)
       st.success("¡Archivo PDF procesado exitosamente!")
       pdf_file.close()
-      st.session_state["messages"].append({"role": "assistant", "content": "Mierda"})
+      
 
 
 
