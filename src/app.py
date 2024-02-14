d0a8445e92c463aad2ffbf66c47cbbce8e63070a
Ciro <cironidas@gmail.com>
2024-02-14 10:25:21 +0100

    a

--- src/app.py
+++ src/app.py
@@ -39,7 +39,7 @@ uploaded_file = st.file_uploader("Sube tu archivo PDF", type="pdf")
 if uploaded_file is not None:
     pdf_bytes = uploaded_file.read()
     pdf_file = BytesIO(pdf_bytes)
-    docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME)
+    #docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME, DIMENSION)
     st.success("¡Archivo PDF procesado exitosamente!")
     pdf_file.close()
 
@@ -53,7 +53,7 @@ if user_input := st.chat_input():
   st.session_state["messages"].append({"role": "user", "content": user_input})
   st.chat_message("user").write(user_input)
 
-  if is_pdf_chatbot and uploaded_file is not None:
+  """if is_pdf_chatbot and uploaded_file is not None:
     responseMessage = pdf_gpt.get_answer(docsearch,user_input,api_key)
   else:
     response = openai.ChatCompletion.create(
@@ -62,8 +62,14 @@ if user_input := st.chat_input():
         engine=GPT_CHAT_ENGINE,
         max_tokens=DIMENSION
     )
-    responseMessage = response['choices'][0]['message']['content']
-
+    responseMessage = response['choices'][0]['message']['content']"""
+  response = openai.ChatCompletion.create(
+        model=GPT_MODEL,
+        messages=st.session_state["messages"],
+        engine=GPT_CHAT_ENGINE,
+        max_tokens=DIMENSION
+    )
+  responseMessage = response['choices'][0]['message']['content']
   st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
   st.chat_message("assistant").write(responseMessage)
 
--- src/pdf_gpt.py
+++ src/pdf_gpt.py
@@ -5,17 +5,19 @@ from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-import pinecone
+from pinecone import Pinecone, ServerlessSpec
 
 
-def process_pdf(pdf_file, openai_api_key, pinecone_api_key,pinecone_api_env,index_name):
+"""def process_pdf(pdf_file, openai_api_key, pinecone_api_key,pinecone_api_env,index_name, dimension):
     with pdfplumber.open(pdf_file) as pdf:
-        pages = [page.extract_text() for page in pdf.pages]
-    PINECONE_API_KEY = pinecone_api_key
-    PINECONE_API_ENV = pinecone_api_env
+        text += [page.extract_text() for page in pdf.pages]
+    
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
-    docsearch = Pinecone.from_texts(pages, embeddings, index_name=index_name)
+    #PINECONE_API_KEY = pinecone_api_key
+    #PINECONE_API_ENV = pinecone_api_env
+    #pc = Pinecone(api_key=PINECONE_API_KEY)
+    # pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+    #docsearch = Pinecone.from_texts(pages, embeddings, index_name=index_name)
     
     return docsearch
 
@@ -26,4 +28,21 @@ def get_answer(docsearch, question, openai_api_key):
     chain = load_qa_chain(llm, chain_type="stuff")
     result = chain.run(input_documents=docs, question=question)
 
-    return result
+    return result"""
+
+def process_pdf(pdf_file, openai_api_key):
+  with pdfplumber.open(pdf_file) as pdf:
+    pages = [page.extract_text() for page in pdf.pages]
+    text = " ".join(pages)
+
+  embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+  return text, embeddings
+
+def get_answer(text, embeddings, question, openai_api_key):
+  docs = similarity_search(embeddings, question, include_metadata=True)
+
+  llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+  chain = load_qa_chain(llm, chain_type="stuff")
+  result = chain.run(input_documents=docs, question=question)
+
+  return result
