9e53542c02cb455e19be064ef2cd4f60eac7b1ff
Ciro <cironidas@gmail.com>
2024-02-14 12:46:24 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -39,33 +39,32 @@ st.title("Chatbot")
 
 is_pdf_chatbot = st.checkbox("PDF chatbot")
 uploaded_file = st.sidebar.file_uploader("Sube tu archivo PDF", type="pdf")
+
 if uploaded_file is not None:
   pdf_bytes = uploaded_file.read()
   pdf_file = BytesIO(pdf_bytes)
   #docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME, DIMENSION)
   st.success("¡Archivo PDF procesado exitosamente!")
   pdf_file.close()
-      
-
 
 
 if "messages" not in st.session_state:
   st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
+else:
+  for msg in st.session_state["messages"]:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-for msg in st.session_state["messages"]:
-  st.chat_message(msg["role"]).write(msg["content"])
 
-
-if user_input := st.chat_input():
-  st.session_state["messages"].append({"role": "user", "content": user_input})
-  st.chat_message("user").write(user_input)
-  response = openai.ChatCompletion.create(
-      model=GPT_MODEL,
-      messages=st.session_state["messages"],
-      engine=GPT_CHAT_ENGINE,
-      max_tokens=DIMENSION
-  )
-  responseMessage = response['choices'][0]['message']['content']
-  st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
-  st.chat_message("assistant").write(responseMessage)
+  if user_input := st.chat_input():
+    st.session_state["messages"].append({"role": "user", "content": user_input})
+    st.chat_message("user").write(user_input)
+    response = openai.ChatCompletion.create(
+        model=GPT_MODEL,
+        messages=st.session_state["messages"],
+        engine=GPT_CHAT_ENGINE,
+        max_tokens=DIMENSION
+    )
+    responseMessage = response['choices'][0]['message']['content']
+    st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
+    st.chat_message("assistant").write(responseMessage)
   
