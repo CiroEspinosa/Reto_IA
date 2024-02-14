f786366442912c6b6c192bb25f3943c1eeed0135
Ciro <cironidas@gmail.com>
2024-02-14 13:03:09 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -48,25 +48,25 @@ if uploaded_file is not None:
   pdf_file.close()
 
 
-st.write()
 if "messages" not in st.session_state:
   st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
+
+try:
+  for msg in st.session_state["messages"]:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+  if user_input := st.chat_input():
+    st.session_state["messages"].append({"role": "user", "content": user_input})
+    st.chat_message("user").write(user_input)
+    response = openai.ChatCompletion.create(
+        model=GPT_MODEL,
+        messages=st.session_state["messages"],
+        engine=GPT_CHAT_ENGINE,
+        max_tokens=DIMENSION
+    )
+    responseMessage = response['choices'][0]['message']['content']
+    st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
+    st.chat_message("assistant").write(responseMessage)
+except Exception as e:
   st.write()
-
-st.write()
-for msg in st.session_state["messages"]:
-  st.chat_message(msg["role"]).write(msg["content"])
-
-if user_input := st.chat_input():
-  st.session_state["messages"].append({"role": "user", "content": user_input})
-  st.chat_message("user").write(user_input)
-  response = openai.ChatCompletion.create(
-      model=GPT_MODEL,
-      messages=st.session_state["messages"],
-      engine=GPT_CHAT_ENGINE,
-      max_tokens=DIMENSION
-  )
-  responseMessage = response['choices'][0]['message']['content']
-  st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
-  st.chat_message("assistant").write(responseMessage)
   
