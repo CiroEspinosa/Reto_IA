4d6650994a73698a20d265292e3e5e53438654fa
Ciro <cironidas@gmail.com>
2024-02-14 12:39:42 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -39,9 +39,7 @@ st.title("Chatbot")
 
 is_pdf_chatbot = st.checkbox("PDF chatbot")
 uploaded_file = st.sidebar.file_uploader("Sube tu archivo PDF", type="pdf")
-
-
-if is_pdf_chatbot:
+if uploaded_file is not None:
   pdf_bytes = uploaded_file.read()
   pdf_file = BytesIO(pdf_bytes)
   #docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME, DIMENSION)
