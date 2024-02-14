fdcd11d737dc8e0e989c90bce5e9ce406f8dd732
Ciro <cironidas@gmail.com>
2024-02-14 12:37:18 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -32,39 +32,42 @@ GPT_CHAT_ENGINE = "gepeto"
 # GPT_CHAT_ENGINE = "dictador"
 
 
+  
 
-
-if "messages" not in st.session_state:
-  st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
 
 st.title("Chatbot")
 
 is_pdf_chatbot = st.checkbox("PDF chatbot")
 uploaded_file = st.sidebar.file_uploader("Sube tu archivo PDF", type="pdf")
 
-if uploaded_file is not None:
-    pdf_bytes = uploaded_file.read()
-    pdf_file = BytesIO(pdf_bytes)
-    docsearch = pdf_gpt.process_pdf(pdf_file, api_key, PINECONE_API_KEY, PINECONE_ENV, PINECONE_INDEX_NAME, DIMENSION)
-    st.success("¡Archivo PDF procesado exitosamente!")
-    pdf_file.close()
+
+if is_pdf_chatbot:
+  pdf_bytes = uploaded_file.read()
+  pdf_file = BytesIO(pdf_bytes)
+  docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME, DIMENSION)
+  st.success("¡Archivo PDF procesado exitosamente!")
+  pdf_file.close()
+      
+
+
+
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
+
+for msg in st.session_state["messages"]:
+  st.chat_message(msg["role"]).write(msg["content"])
 
 
 if user_input := st.chat_input():
   st.session_state["messages"].append({"role": "user", "content": user_input})
   st.chat_message("user").write(user_input)
-
-  if is_pdf_chatbot and uploaded_file is not None:
-      responseMessage = pdf_gpt.get_answer(docsearch, user_input, api_key)
-  else:
-      response = openai.ChatCompletion.create(
-          model=GPT_MODEL,
-          messages=st.session_state["messages"],
-          engine=GPT_CHAT_ENGINE,
-          max_tokens=DIMENSION
-      )
-      responseMessage = response['choices'][0]['message']['content']
-
+  response = openai.ChatCompletion.create(
+      model=GPT_MODEL,
+      messages=st.session_state["messages"],
+      engine=GPT_CHAT_ENGINE,
+      max_tokens=DIMENSION
+  )
+  responseMessage = response['choices'][0]['message']['content']
   st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
   st.chat_message("assistant").write(responseMessage)
-
+  
